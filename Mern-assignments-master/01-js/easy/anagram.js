9486043358a0a178d18dc583d11c31c6654e0e12
Vineet <vivekbaghel9719@gmail.com>
2026-01-28 23:05:48 +0530

    Updated anagram solution

--- Mern-assignments-master/01-js/easy/anagram.js
+++ Mern-assignments-master/01-js/easy/anagram.js
@@ -5,7 +5,15 @@
 */
 
 function isAnagram(str1, str2) {
-
+  if(str1.length !== str2.length) {
+    return false;
+  }
+  for(let i = 0;i<str1.length;i++) {
+    if(str2.indexOf(str1[i]) === -1) {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = isAnagram;
