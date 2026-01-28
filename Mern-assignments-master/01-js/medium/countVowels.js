5765b936c4099868df11a430f7a06a9cfda441b5
Vineet <vivekbaghel9719@gmail.com>
2026-01-28 23:32:34 +0530

    01-js easy level complete

--- Mern-assignments-master/01-js/medium/countVowels.js
+++ Mern-assignments-master/01-js/medium/countVowels.js
@@ -7,6 +7,14 @@
 
 function countVowels(str) {
     // Your code here
+    // const vowels = 'aeiouAEIOU';
+    // let count = 0;
+    // for (let i = 0; i < str.length; i++) {
+    //     if (vowels.indexOf(str[i]) !== -1) {
+    //         count++;
+    //     }
+    // }
 }
-
+// const vowelCount = countVowels("Hello World");
+console.log(vowelCount);
 module.exports = countVowels;
